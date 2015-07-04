4d61a18b8540be8bd5b373888066046274fb6f92
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-04 12:34:01 +0200

    Add CI

--- server-auth/src/test-integration/resources/GebConfig.groovy
+++ server-auth/src/test-integration/resources/GebConfig.groovy
@@ -1,8 +1,10 @@
 
+driver = "firefox"
 
 waiting { timeout = 2 }
 
 environments {
+    phantomjs { driver = "org.openqa.selenium.phantomjs.PhantomJSDriver" }
     firefox { driver = "firefox" }
     chrome { driver = "chrome" }
 }
