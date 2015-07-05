b81626b125c435d4cf26ffe2843f318fde8869f1
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-05 11:02:56 +0200

    Fix script: Publish artifacts also to gh-pages

--- tools/publish.sh
+++ tools/publish.sh
@@ -2,9 +2,12 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "hwolf/oauth2" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  echo -e "Publishing reports...\n"
+  echo -e "Publishing artifacts and reports...\n"
 
+  mkdir -p $HOME/reports-latest
   cp -R build/reports $HOME/reports-latest
+  mkdir -p $HOME/artifacts-latest
+  cp -R server-*/build/libs/*.jar $HOME/artifacts-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -15,9 +18,15 @@ if [ "$TRAVIS_REPO_SLUG" == "hwolf/oauth2" ] && [ "$TRAVIS_PULL_REQUEST" == "fal
   git rm -rf ./reports
   cp -Rf $HOME/reports-latest ./reports
   git add -f .
-  git commit -m "Lastest reports on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+
+  git rm -rf ./artifacts
+  cp -Rf $HOME/artifacts-latest ./artifacts
+  git add -f .
+
+  git commit -m "Lastest artifacts and reports on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published reports to gh-pages.\n"
+  echo -e "Published artifacts and reports to gh-pages.\n"
   
 fi
+
