bac72c206828eb54f3e7461084e2ca23e8f80c88
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-05 06:22:06 +0200

    Fix repository name

--- tools/publish.sh
+++ tools/publish.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "hwolf/oauth2" ] && [ "$TRAVIS_PULL_REQUEST" == "fal
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hwolf/oauth2 gh-pages > /dev/null
 
   cd gh-pages
   git rm -rf ./reports
