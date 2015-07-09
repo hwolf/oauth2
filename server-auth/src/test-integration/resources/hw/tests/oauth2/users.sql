d757e4a78299db4fca5255686661a7040ea8fe75
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-09 20:55:55 +0200

    Fix integration tests in CI

--- server-auth/src/test-integration/resources/hw/tests/oauth2/users.sql
+++ server-auth/src/test-integration/resources/hw/tests/oauth2/users.sql
@@ -1,7 +1,10 @@
 
-delete from t_user_authorities where username = 'admin1';
-delete from t_users where username = 'admin1';
+delete from t_login_status where user_id = 'admin1';
+delete from t_user_entries where user_id = 'admin1';
+delete from t_users where user_id = 'admin1';
 
-insert into t_users (username, password, password_expired, login_attempts)
-	values ('admin1', 'admin1', ADD_MONTHS(now(), 2), 0);	
-insert into t_user_authorities (username, authority) values ('admin1', 'ROLE_ADMIN');
+insert into t_users (user_id, password, password_expired)
+	values ('admin1', 'admin1', DATE_ADD(now(), interval 100 day));	
+insert into t_user_entries (user_id, name, data) values ('admin1', 'ROLE', 'ADMIN');
+
+insert into t_login_status (user_id, failed_login_attempts) value ('admin1', 0);
