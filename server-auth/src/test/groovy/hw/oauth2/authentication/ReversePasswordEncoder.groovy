9f9a374cee3429abffe5f382bed07da969e5d660
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-11 12:08:19 +0200

    Add docu

--- server-auth/src/test/groovy/hw/oauth2/authentication/ReversePasswordEncoder.groovy
+++ server-auth/src/test/groovy/hw/oauth2/authentication/ReversePasswordEncoder.groovy
@@ -2,6 +2,11 @@ package hw.oauth2.authentication
 
 import org.springframework.security.crypto.password.PasswordEncoder
 
+/**
+ * A simple password encoder which encodes a password by reversing the password.
+ *
+ * This is just for testing purpose to distinguish a raw password and an encoded password.
+ */
 class ReversePasswordEncoder implements PasswordEncoder {
 
     String encode(CharSequence rawPassword) {
