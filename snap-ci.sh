35c3c986ad53debcb8cdb39984dd3f040f8dae9c
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-12 23:24:36 +0200

    Imporve script

--- snap-ci.sh
+++ snap-ci.sh
@@ -5,6 +5,8 @@ export SPRING_DATASOURCE_PASSWORD=$SNAP_DB_MYSQL_PASSWORD
 export SPRING_DATASOURCE_URL=$SNAP_DB_MYSQL_JDBC_URL
 export SPRING_DATASOURCE_DRIVERCLASSNAME=org.mariadb.jdbc.Driver
 
+export CODECOV_TOKEN
+
 ./gradlew --version
 ./gradlew --stacktrace clean ciBuild
 bash <(curl -s https://codecov.io/bash)
