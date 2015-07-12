c115a6083ef29e75e42192272d848538531779c8
Heiko Wolf <wolf.heiko@gmx.net>
2015-07-12 22:34:51 +0200

    Try to fix mysql database problem

--- server-auth/src/main/resources/db/migration/V1_20150712_03__approvals.sql
+++ server-auth/src/main/resources/db/migration/V1_20150712_03__approvals.sql
@@ -4,7 +4,7 @@ create table t_approvals (
 	client_id varchar(20) not null,
 	scope varchar(50) not null,
 	status varchar(10) not null,
-	expires_at timestamp not null,
-	last_update_at timestamp not null,
+	expires_at timestamp,
+	last_update_at timestamp,
 	primary key (user_id, client_id, scope)
 );
