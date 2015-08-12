a07324b56fcc016cb88c64eb37d9903cabbe1ba5
Heiko Wolf <wolf.heiko@gmx.net>
2015-08-12 23:18:58 +0200

    Cleanup

--- server-auth/src/test/groovy/hw/oauth2/authentication/tokens/TokenStoreImplTest.groovy
+++ server-auth/src/test/groovy/hw/oauth2/authentication/tokens/TokenStoreImplTest.groovy
@@ -1,9 +1,5 @@
 package hw.oauth2.authentication.tokens
 
-import hw.oauth2.entities.AccessToken
-import hw.oauth2.entities.AccessTokenRepository
-import hw.oauth2.entities.RefreshTokenRepository
-
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 import javax.sql.DataSource
@@ -25,13 +21,15 @@ import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
 
+import hw.oauth2.entities.AccessToken
+import hw.oauth2.entities.AccessTokenRepository
+import hw.oauth2.entities.RefreshTokenRepository
+
 @RunWith(SpringJUnit4ClassRunner)
-//@IntegrationTest
 @ContextConfiguration(classes = TestConfiguration)
 @ActiveProfiles("dev")
 class TokenStoreImplTest extends TokenStoreBaseTests {
 
-    //    @Configuration
     @EnableAutoConfiguration
     @EntityScan(basePackageClasses = AccessToken)
     @EnableJpaRepositories(basePackageClasses = AccessTokenRepository)
@@ -76,29 +74,4 @@ class TokenStoreImplTest extends TokenStoreBaseTests {
     TokenServiceImpl getTokenStore() {
         return tokenStore
     }
-
-    //    @Before
-    //    void setUp() throws Exception {
-    //        // creates a HSQL in-memory db populated from default scripts classpath:schema.sql and classpath:data.sql
-    //        db = new EmbeddedDatabaseBuilder().addDefaultScripts().build()
-    //
-    //        AccessTokenRepository repo1 = new Acc
-    //        tokenStore = new TokenServiceImpl(db)
-    //    }
-    //
-    //    @Test
-    //    void testFindAccessTokensByUserName() {
-    //        OAuth2Authentication expectedAuthentication = new OAuth2Authentication(RequestTokenFactory.createOAuth2Request("id", false),
-    //                new TokenStoreBaseTests.TestAuthentication("test2", false))
-    //        OAuth2AccessToken expectedOAuth2AccessToken = new DefaultOAuth2AccessToken("testToken")
-    //        getTokenStore().storeAccessToken(expectedOAuth2AccessToken, expectedAuthentication)
-    //
-    //        Collection<OAuth2AccessToken> actualOAuth2AccessTokens = getTokenStore().findTokensByUserName("test2")
-    //        assert actualOAuth2AccessTokens.size()  == 1
-    //    }
-    //
-    //    @After
-    //    public void tearDown() throws Exception {
-    //        db.shutdown()
-    //    }
 }
