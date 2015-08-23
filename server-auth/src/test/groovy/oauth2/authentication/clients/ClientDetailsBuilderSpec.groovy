ff977ca23f4ded43949ac31770da0046b66eedbd
Heiko Wolf <wolf.heiko@gmx.net>
2015-08-23 16:17:06 +0200

    Add header

--- server-auth/src/test/groovy/oauth2/authentication/clients/ClientDetailsBuilderSpec.groovy
+++ server-auth/src/test/groovy/oauth2/authentication/clients/ClientDetailsBuilderSpec.groovy
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2015 H. Wolf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package oauth2.authentication.clients
 
 import org.springframework.security.oauth2.provider.ClientDetails
 
-import oauth2.authentication.clients.ClientDetailsBuilder;
 import spock.lang.Specification
 
 class ClientDetailsBuilderSpec extends Specification {
@@ -124,9 +138,7 @@ class ClientDetailsBuilderSpec extends Specification {
         ClientDetails client = clientDetailsBuilder().withRole(role1).withRole(role2).build()
 
         then:
-        client.authorities.authority as Set == [
-            "ROLE_" + role1,
-            "ROLE_" + role2] as Set
+        client.authorities.authority as Set == ["ROLE_" + role1, "ROLE_" + role2] as Set
     }
 
     def "set resource ids"() {
